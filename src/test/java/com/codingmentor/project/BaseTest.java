b5f661b6d7c64c4e7c5e5a3151faa2ecb40ac269
mdbasheer333 <mdbasheer333@gmail.com>
2022-08-25 10:04:45 +0530

    added cmd prop

--- src/test/java/com/codingmentor/project/BaseTest.java
+++ src/test/java/com/codingmentor/project/BaseTest.java
@@ -18,9 +18,20 @@ public class BaseTest {
 	public void tearUp(String browserType) throws Exception {
 		
 		String browTy=PropertyUtil.getProperty("browerToSelect");
+		String br= System.getProperty("browser");
+		
+		System.out.println("command line value is " + br);
+		
+		System.out.println("--------------- " + System.getProperty("name"));
+		System.out.println("----------- " + System.getProperty("city"));
+		System.out.println("----------- " + System.getProperty("age"));
 		
 		if(browTy!=null) {
 			browserType=browTy;
+		}
+		
+		if(br!=null) {
+			browserType=br;
 		}
 		
 		switch (browserType) {
@@ -36,10 +47,39 @@ public class BaseTest {
 			driver.get(PropertyUtil.getProperty("appUrl"));
 			driver.manage().window().maximize();
 			break;
+			
+//		case "headless":
+//			System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\drivers\\msedgedriver.exe");
+//			driver = new HtmlUnitDriver();
+//			driver.get(PropertyUtil.getProperty("appUrl"));
+//			driver.manage().window().maximize();
+//			break;
+			
 		default:
 			throw new Exception("pls select valid broswr");
 		}
 	}
+	
+//	@BeforeMethod
+//	public void tearUpOnlyCommandLine() throws Exception {
+//		String browserType= System.getProperty("browser");
+//		switch (browserType) {
+//		case "chrome":
+//			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\drivers\\chromedriver.exe");
+//			driver = new ChromeDriver();
+//			driver.get(PropertyUtil.getProperty("appUrl"));
+//			driver.manage().window().maximize();
+//			break;
+//		case "edge":
+//			System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\drivers\\msedgedriver.exe");
+//			driver = new EdgeDriver();
+//			driver.get(PropertyUtil.getProperty("appUrl"));
+//			driver.manage().window().maximize();
+//			break;
+//		default:
+//			throw new Exception("pls select valid broswr");
+//		}
+//	}
 	
 	@AfterMethod
 	public void tearDown() {
